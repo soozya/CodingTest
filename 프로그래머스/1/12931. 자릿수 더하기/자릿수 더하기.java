d9dcba774a44f1d67224dcd95eecd12f4ae28007
soozya <122772589+soozya@users.noreply.github.com>
2023-11-17 20:58:01 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.13 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -8,6 +8,8 @@ public class Solution {
             answer += n%10 ;
             n /=10;
         }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
         return answer;
     }
