de461faa25d7167c1fa8ccb5f457324d932e1f91
soozya <122772589+soozya@users.noreply.github.com>
2023-11-21 21:55:47 +0900

    Update 정수 제곱근 판별.java

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,6 +1,5 @@
 class Solution {
     public long solution(long n) {
-        long answer=0;
         long num=(long)Math.sqrt(n);
         
         return (long)Math.pow(num,2)==n ? (long)Math.pow(num+1,2) : -1;
